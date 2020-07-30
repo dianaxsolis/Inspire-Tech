49f35cf3e86567c56bd5814fd2d98f03a413e89c
Diana Solis <solisdiana2603@gmail.com>
2020-07-30 10:54:08 -0500

    future page changes v2

--- Inspire Tech/futureViewController.swift
+++ Inspire Tech/futureViewController.swift
@@ -18,13 +18,20 @@ class futureViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func ELEMENTARY(_ sender: Any) {
+        
+        LABELINFO.text = "You're in the beginning! Try thinking of what classes make you excited. Do you really like doing experiments or the multiplication tables? Make sure you're learning the material as it is the core of anything you will do."
     }
     @IBAction func MIDDLE(_ sender: Any) {
+        LABELINFO.text = "Middle school can be hard! Start looking into activities that you can participate in, such as coding clubs and more. As well, when choosing your high school schedule, pick classes that might not be in your comfort zone like CS."
     }
     
     @IBAction func HIGHSCHOO(_ sender: Any) {
+        
+        LABELINFO.text = "This is the peak! Continue working hard in your classes and start working towards higher level STEM classes. Engage in opportunities such as in our Opportunities page like KWK and more. Look into colleges that offer STEM majors and start thinking about your career."
     }
     
+    @IBAction func HOMEBUTTON(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
